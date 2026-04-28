f81a31a99ada03eaa6fc8949f3d76aa185c4df0c
LeonardoVechi <leonardo_vechi19@hotmail.com>
2026-04-27 22:29:33 -0300

    Commit inicial do projeto para um task manager, adicionado CRUD para usuários com registro e login publicos para realizacao da autenticação jwt e password com hash

--- ProjetoTaskManager.Infrastructure/obj/Debug/net9.0/ProjetoTaskManager.Infrastructure.AssemblyInfo.cs
+++ ProjetoTaskManager.Infrastructure/obj/Debug/net9.0/ProjetoTaskManager.Infrastructure.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ProjetoTaskManager.Infrastructure")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0382d5eb17247c9da4eefa31971e62f12781adfb")]
 [assembly: System.Reflection.AssemblyProductAttribute("ProjetoTaskManager.Infrastructure")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ProjetoTaskManager.Infrastructure")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
